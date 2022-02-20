f57ff0c12611f02d8dcbfb5d440211e74151b143
Deividwb <bernardinodeivid@gmail.com>
2022-02-20 20:23:27 -0300

    Styling Css Navbar

--- frontweb_catalogcar/src/components/Navbar/index.tsx
+++ frontweb_catalogcar/src/components/Navbar/index.tsx
@@ -1,28 +1,27 @@
+import './styles.css';
 import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
-    return(
-        <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
-            <div className="container-fluid">
-                <Link to="/" className="nav-logo-text" >
-                <h4>Carros top</h4>
-                </Link>
-                <div>
-                    <ul>
-                        <li>
-                            <NavLink to="/" activeClassName="active">
-                            Home
-                            </NavLink>
-                        </li>
-                        <li>
-                        <NavLink to="/products" activeClassName="active">
-                            Catalógo
-                            </NavLink>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
-}
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
+      <div className="container-fluid">
+        <Link to="/" className="nav-logo-text">
+          <h4>Carros top</h4>
+        </Link>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav offset-md-2 main-menu">
+            <li>
+              <NavLink to="/" activeClassName="active">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/products">Catalógo</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 export default Navbar;
